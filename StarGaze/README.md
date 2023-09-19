4d423ab1bf9e0f8d1287d131792a4cb9efbf1d6c
gjheij <j.heij@spinozacentre.nl>
2023-09-19 21:52:03 +0200

    helper functions in utils.py

--- StarGaze/README.md
+++ StarGaze/README.md
@@ -2,8 +2,8 @@
 
 Saccade/smooth pursuit experiment where subjects follow a circle on the screen in two ways: 1) in multiple steps from A to B, or a smooth transit from A to B, following a star pattern. 
 
-```python main.py <sub ID> <ses ID> <run ID>```
+```python main.py <sub ID> <ses ID> <run ID> <condition name>```
 
 e.g.,:
 
-```python main.py 01 1 1```
+```python main.py 01 1 1 gaze```
--- StarGaze/session.py
+++ StarGaze/session.py
@@ -1,11 +1,9 @@
 from exptools2.core import Session
 import numpy as np
-import pandas as pd
-from psychopy import tools, logging
-import scipy.stats as ss
 from stimuli import StarStim
 from trial import StarTrial, DummyWaiterTrial, OutroTrial
 import os
+from utils import *
 opj = os.path.join
 opd = os.path.dirname
 
@@ -177,105 +175,3 @@ class StarSession(Session):
             trial.run()
 
         self.close()
-
-def slope(dx, dy):
-    return (dy / dx) if dx else None
-
-class Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return '({}, {})'.format(self.x, self.y)
-
-    def __repr__(self):
-        return 'Point({}, {})'.format(self.x, self.y)
-
-    def halfway(self, target):
-        midx = (self.x + target.x) / 2
-        midy = (self.y + target.y) / 2
-        return Point(midx, midy)
-
-    def distance(self, target):
-        dx = target.x - self.x
-        dy = target.y - self.y
-        return (dx*dx + dy*dy) ** 0.5
-
-    def reflect_x(self):
-        return Point(-self.x,self.y)
-
-    def reflect_y(self):
-        return Point(self.x,-self.y)
-
-    def reflect_x_y(self):
-        return Point(-self.x, -self.y)
-
-    def slope_from_origin(self):
-        return slope(self.x, self.y)
-
-    def slope(self, target):
-        return slope(target.x - self.x, target.y - self.y)
-
-    def y_int(self, target):       # <= here's the magic
-        return self.y - self.slope(target)*self.x
-
-    def line_equation(self, target):
-        slope = self.slope(target)
-
-        y_int = self.y_int(target)
-        if y_int < 0:
-            y_int = -y_int
-            sign = '-'
-        else:
-            sign = '+'
-
-        return 'y = {}x {} {}'.format(slope, sign, y_int)
-
-    def line_function(self, target):
-        slope = self.slope(target)
-        y_int = self.y_int(target)
-        def fn(x):
-            return slope*x + y_int
-        return fn
-
-def get_positions(pos, n_repeats):
-
-    all_pos = pos
-    for _ in range(n_repeats-1):
-        all_pos += pos
-
-    return all_pos
-
-
-# iti function based on negative exponential
-def _return_itis(mean_duration, minimal_duration, maximal_duration, n_trials):
-    itis = np.random.exponential(scale=mean_duration-minimal_duration, size=n_trials)
-    itis += minimal_duration
-    itis[itis>maximal_duration] = maximal_duration
-    return itis
-
-def iterative_itis(mean_duration=6, minimal_duration=3, maximal_duration=18, n_trials=None, leeway=0, verbose=False):
-    
-    nits = 0
-    itis = _return_itis(
-        mean_duration=mean_duration,
-        minimal_duration=minimal_duration,
-        maximal_duration=maximal_duration,
-        n_trials=n_trials)
-
-    total_iti_duration = n_trials * mean_duration
-    min_iti_duration = total_iti_duration - leeway
-    max_iti_duration = total_iti_duration + leeway
-    while (itis.sum() < min_iti_duration) | (itis.sum() > max_iti_duration):
-        itis = _return_itis(
-            mean_duration=mean_duration,
-            minimal_duration=minimal_duration,
-            maximal_duration=maximal_duration,
-            n_trials=n_trials)
-        nits += 1
-
-    if verbose:
-        print(f'ITIs created with total ITI duration of {round(itis.sum(),2)}s after {nits} iterations')    
-
-    return itis
